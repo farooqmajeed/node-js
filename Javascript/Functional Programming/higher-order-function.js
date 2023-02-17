1fc5fb4448122859afdb91654b30290c06ac348d
farooqmajeed <farooq306@hotmail.com>
2023-02-17 23:06:23 +0500

    composition and pipe

--- Javascript/Functional Programming/higher-order-function.js
+++ Javascript/Functional Programming/higher-order-function.js
@@ -14,3 +14,15 @@ const toLowercase = str =str.toLowercase();
 
 // trim(input);
 const result  = wrapInDiv(toLowercase(trim(input))); 
+
+// you can see the function composition but here two many brackets with this  function
+
+// composing and piping
+// for doing that we need to install lodash
+import {compose, pipe} from 'lodash/fp';
+
+const transform = compose(wrapInDiv, toLowercase, trim) ;
+transform(input);
+// for sequence
+const transformPipe = pipe(  trim, wrapInDiv,toLowercase); //here it first trim input then  tolowercase then wrap in div
+transformPipe(input)
