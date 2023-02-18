c183f4cd7c860f2573f35d65b02fb09352556750
zeeshanshanif <zee81zee@yahoo.com>
2023-02-18 14:27:03 +0500

    updating object

--- Javascript/Functional Programming/higher-order-function.js
+++ Javascript/Functional Programming/higher-order-function.js
@@ -1,4 +1,4 @@
-let input  = " JavaScript ";
+let input = " JavaScript ";
 let output = "<div>" + input.trim() + "</div>";
 
 
@@ -6,25 +6,25 @@ let output = "<div>" + input.trim() + "</div>";
 //  trim
 // wrapInDiv
 
-const trim = str =>  str.trim();
+const trim = str => str.trim();
 const wrapInDiv = str => `<div> ${str}</div>`
-const toLowercase = str =str.toLowercase();
+const toLowercase = str = str.toLowercase();
 
 // these two are reusable function
 
 // trim(input);
-const result  = wrapInDiv(toLowercase(trim(input))); 
+const result = wrapInDiv(toLowercase(trim(input)));
 
 // you can see the function composition but here two many brackets with this  function
 
 // composing and piping
 // for doing that we need to install lodash
-import {compose, pipe} from 'lodash/fp';
+import { compose, pipe } from 'lodash/fp';
 
-const transform = compose(wrapInDiv, toLowercase, trim) ;
+const transform = compose(wrapInDiv, toLowercase, trim);
 transform(input);
 // for sequence
-const transformPipe = pipe(  trim, wrapInDiv,toLowercase); //here it first trim input then  tolowercase then wrap in div
+const transformPipe = pipe(trim, wrapInDiv, toLowercase); //here it first trim input then  tolowercase then wrap in div
 transformPipe(input)
 
 
@@ -33,3 +33,27 @@ const wrap = type => str => `<${type}>${str}</${type}>`
 
 const transformCurr = pipe(trim, toLowercase, wrap("div"))
 console.log(transformCurr(input));
+
+
+
+// updating Object
+// we have two methods to update the object
+// 1st way
+const person = { name: "Usama" };
+const updatePerson = Object.assign({}, person, { name: "Sajid", age: "30" });
+console.log(updatePerson)
+
+// 2nd way
+const person2 = { name: "Usama" };
+const updatePerson2 = { ...person, name: "Farooq" };
+console.log(updatePerson)
+// be careful when using spread operator in nested objects because it creates a shallow copy of the object
+//  make deep copy of object
+
+const person3 = { name: "Usama", address: {
+    country:"Pakistan",
+    city :"karachi"
+} };
+
+const updatePerson3 = { ...person, address:{...address, city:"islamabad"}, name: "Farooq" };
+console.log(updatePerson)
