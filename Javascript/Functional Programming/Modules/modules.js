8f6817606c4ab5f0c20baa87ee3d6c4decb6d82e
farooqmajeed <farooq306@hotmail.com>
2023-02-23 14:55:32 +0500

    named and default exports

--- Javascript/Functional Programming/Modules/modules.js
+++ Javascript/Functional Programming/Modules/modules.js
@@ -6,3 +6,8 @@ const person = new Person("John");
 
 teacher.tech(); // returns teach
 person.walk(); // returns walk
+
+// export in javascript
+// 1) Named export  -> import {...} from '';
+// 2) Default export -> import ... from '';
+//  we can also export both named and default like this import ... , {...};
