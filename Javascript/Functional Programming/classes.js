dc0d8fcaf68fce7c96f8f4aa215ef03506787242
farooqmajeed <farooq306@hotmail.com>
2023-02-23 13:35:32 +0500

    classes in javascript

---
+++ Javascript/Functional Programming/classes.js
@@ -0,0 +1,17 @@
+const person = {
+    name:"John",
+    walk(){
+        console.log(walk);
+    }
+};
+
+// when we  object with atleast one method we need to create a blue print of object of that type
+
+class Person {
+    constructor(){
+        
+    }
+    walk(){
+        console.log("walk");
+    }
+}
