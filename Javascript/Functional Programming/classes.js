f7f37430e92e644fb1ed3399c68a49bc13558f80
farooqmajeed <farooq306@hotmail.com>
2023-02-23 13:58:04 +0500

    create class

--- Javascript/Functional Programming/classes.js
+++ Javascript/Functional Programming/classes.js
@@ -1,17 +1,20 @@
-const person = {
-    name:"John",
-    walk(){
-        console.log(walk);
-    }
-};
+// const person = {
+//     name:"John",
+//     walk(){
+//         console.log(walk);
+//     }
+// };
 
 // when we  object with atleast one method we need to create a blue print of object of that type
 
 class Person {
-    constructor(){
-        
+    constructor(name){
+        this.name = name;
     }
     walk(){
         console.log("walk");
     }
 }
+
+
+const person = new Person('John ');
