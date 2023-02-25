506c390e5520cc6189c17362c1d3fc03994cb8cc
farooqmajeed <farooq306@hotmail.com>
2023-02-25 15:36:24 +0500

    added gitignore

---
+++ buildRestFullApi/firstExpressServer/index.js
@@ -0,0 +1,11 @@
+const export = require('events');
+
+const app = express();
+
+// we have four methods 
+
+// app.get() fetch data from server
+// app.post() send data to server
+// app.put() update data to server
+// app.delete() delete data from server
+// app.patch() update data to server
