61349c00b50ed89f0de8dd9f40fa14220622b733
farooqmajeed <farooq306@hotmail.com>
2023-02-27 00:13:50 +0500

    api to filtered specific course  with id from array

--- buildRestFullApi/firstExpressServer/index.js
+++ buildRestFullApi/firstExpressServer/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-// we have four methods 
+// we have four methods
+
+const courses = [
+  { id: "1", name: "Course1" },
+  { id: "2", name: "Course2" },
+  { id: "3", name: "Course3" },
+];
 
 // app.get() fetch data from server
 // app.post() send data to server
@@ -10,27 +16,31 @@ const app = express();
 // app.delete() delete data from server
 // app.patch() update data to server
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/courses', (req, res) => {
-  res.send((JSON.stringify([1,2,4])));
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
 // for the  specific id of course
-app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+app.get("/api/courses/:id", (req, res) => {
+  console.log(req.params);
+  const course = courses.find((c) => c.id === req.params.id);
+  console.log("====", course);
+  if (!course) res.status(404).send("The course is not available");
+  res.send(course);
 });
 
 // for specific params in url
-app.get('/api/post/:year/:month', (req, res) => {
-  res.send(req.params.id); // return send { year: '2023', month: '1',}
-});
-// for specific query in url
-app.get('/api/post/:year/:month', (req, res) => {
-  res.send(req.query); // return send {sortBy:'name'}
-});
+// app.get('/api/post/:year/:month', (req, res) => {
+//   res.send(req.params.id); // return send { year: '2023', month: '1',}
+// });
+// // for specific query in url
+// app.get('/api/post/:year/:month', (req, res) => {
+//   res.send(req.query); // return send {sortBy:'name'}
+// });
 app.listen(3000, () => {
-console.log('Server');
+  console.log("Server");
 });
