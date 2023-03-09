3b841d61f23645caca0f44d13a5c61ecc457940f
farooqmajeed <farooq306@hotmail.com>
2023-03-09 18:06:13 +0500

    remove header

--- Frontend/React/react-hooks/src/App.js
+++ Frontend/React/react-hooks/src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import UseStateHook from './hooks/useStateHook'
 import ContextHook from './hooks/useContextHook'
+import CallBackHook from './hooks/useCallbackHook';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         React Hooks
       </header>
       <UseStateHook />
-      <ContextHook />
+      <ContextHook /> */}
+      <CallBackHook />
     </div>
   );
 }
