b540b1ce87b7c54f9981c9053d0778647f397075
farooqmajeed <farooq306@hotmail.com>
2023-03-09 18:13:02 +0500

    added increament functionality

--- Frontend/React/react-hooks/src/hooks/useCallbackHook.js
+++ Frontend/React/react-hooks/src/hooks/useCallbackHook.js
@@ -1,23 +1,28 @@
-import React, {useCallback, useState} from 'react'
-import { Todos } from './todos';
+import React, { useCallback, useState } from "react";
+import { Todos } from "./todos";
 
 const CallBackHook = () => {
-    const [count, setCount] = useState(0);
-    const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = (todo) =>{
-        setTodos((prev) => [...prev, 'New Entry'])
-    }
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev, "New Entry"]);
+  };
+
+  const increament = () => {
+    setCount(count + 1);
+  }
 
   return (
     <div>
-        <header className="App-header">
-        React useCallback Hooks
-      </header>
-        <Todos todos={todos} addTodo={addTodo}/>
-    
+      <header className="App-header">React useCallback Hooks</header>
+      <Todos todos={todos} addTodo={addTodo} />
+      <div>
+        count:{count}
+        <button onClick={increament}> +</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CallBackHook
+export default CallBackHook;
