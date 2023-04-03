e00adbc4d4d1d0dfd9d5fa35a964d631ca44e2cd
farooqmajeed <farooq306@hotmail.com>
2023-04-04 01:45:20 +0500

    added next google font

--- Next/my-app/app/about/page.jsx
+++ Next/my-app/app/about/page.jsx
@@ -2,7 +2,7 @@ export default function About(){
     // throw new Error('test')
     return (
         <div>
-            <h1> About page</h1>
+            <h1  className="text-lg py-2 m-5"> About page</h1>
         </div>
     )
 }
