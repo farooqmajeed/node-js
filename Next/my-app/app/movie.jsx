94311ebfbf38845d4eab2278bf31507986161a65
farooqmajeed <farooq306@hotmail.com>
2023-04-04 04:03:49 +0500

    added generateStaticParams

--- Next/my-app/app/movie.jsx
+++ Next/my-app/app/movie.jsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Movies({ key, id, title, release_date, poster_path }) {
-    const imagePath = "https://image.tmdb.org/t/p/original"
+  const imagePath = "https://image.tmdb.org/t/p/original";
 
   return (
     <div>
       <h1>{title}</h1>
       <h2>{release_date}</h2>
-
       <Link href={`/${id}`}>
-        <Image  src={imagePath + poster_path} alt={title} width={800} height={800}/>
+        <Image
+          src={imagePath + poster_path}
+          alt={title}
+          width={800}
+          height={800}
+        />
       </Link>
     </div>
   );
--- Next/my-app/app/page.jsx
+++ Next/my-app/app/page.jsx
@@ -1,11 +1,4 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
-// import styles from "./page.module.css";
-
 import Movies from "./movie";
-
-// const inter = Inter({ subsets: ["latin"] });
-
 export default async function Home() {
   const data = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
