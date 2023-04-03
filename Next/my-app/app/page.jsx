74c1000572834c0d605c402ae49a3eab5f5ae99f
farooqmajeed <farooq306@hotmail.com>
2023-04-04 02:41:43 +0500

    added the imdb api to fetch popular movies

--- Next/my-app/app/page.jsx
+++ Next/my-app/app/page.jsx
@@ -4,10 +4,22 @@
 
 // const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log("res", res);
+
   return (
     <main>
-      <h1>hello next 13</h1>
+      {/* <h1>hello next 13</h1> */}
+
+      {res.results.map((movie) => (
+        <div>
+          <h1>{movie.title}</h1>
+        </div>
+      ))}
     </main>
   );
 }
