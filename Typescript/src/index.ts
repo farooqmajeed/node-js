35f6a6d5b7b4f5fb2aee560387a39db5f9f165e8
farooqmajeed <farooq306@hotmail.com>
2023-04-17 01:59:10 +0500

    added objects

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -2,13 +2,31 @@
 
 let age: number = 20;
 // age = 'a''  showing type error message
-if(age < 50){
-    age+= 10;
+if (age < 50) {
+  age += 10;
 }
-
 
 // functions in typescript
 //  here below the String means  the function return type
-function calculateTax(tax: number) :String {
-    return tax;
+function calculateTax(tax: number): String {
+  return tax;
 }
+//  if user does not have need to send  multiple parameters we can make them optional(arg?: number) or we can also
+//  pass them default value like (args: 2000)
+
+// Object in javascript
+
+let employee = { id: 1 }; // this will create a new object with shape (id: number)
+employee.name = "javascript"; // valid in javascript but not in TypeScript
+
+let employeeTS: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "TypeScript",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
