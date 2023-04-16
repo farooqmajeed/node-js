798d6a863e8ca5770f4524de9b7f3501ceaeddd0
farooqmajeed <farooq306@hotmail.com>
2023-04-17 01:18:41 +0500

    debugging typescript

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -2,3 +2,4 @@
 // intializing number
 let age = 20;
 // age = 'a''  showing type error message
+//# sourceMappingURL=index.js.map
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -2,3 +2,6 @@
 
 let age: number = 20;
 // age = 'a''  showing type error message
+if(age < 50){
+    age+= 10;
+}
